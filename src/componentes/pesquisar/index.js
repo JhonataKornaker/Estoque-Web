7c8001be5d29ea66df12f072311224d35333a324
Jhonata Kornaker <jhonataalberto@outlook.com>
2023-12-29 17:06:59 -0300

    Adicionando componente pesquisar

---
+++ src/componentes/pesquisar/index.js
@@ -0,0 +1,11 @@
+import './Pesquisar.css'
+
+const Pesquisar = () => {
+    return (
+        <div className='busca'>
+            <input type='text' placeholder='Digite o texto para busca' />
+        </div>
+    )
+}
+
+export default Pesquisar;
